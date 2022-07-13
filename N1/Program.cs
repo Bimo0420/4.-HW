93e487bc5795f7d3d104068ea348dc6ec5853fe7
Алексей Викулин <vaa@megamade.ru>
2022-07-13 09:58:59 +0300

    00

--- N1/Program.cs
+++ N1/Program.cs
@@ -10,6 +10,19 @@ namespace N1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите целое число N > 0");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n>0)
+            {
+                for (int i = 1; i < n; i++)
+                
+                Console.WriteLine("Квадрат чисел от 1 до {0} равен: {1}", n, i*i);
+            }
+            else
+            {
+                Console.WriteLine("Не верный ввод");
+            }
+            Console.ReadKey();
         }
     }
 }
